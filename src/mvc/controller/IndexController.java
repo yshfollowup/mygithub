b82ef96fda21efae9b255fb0af93028d02278927
admin <admin@1104-25-PC>
2018-03-08 10:43:46 +0900

    project ver 0.1.0

---
+++ src/mvc/controller/IndexController.java
@@ -0,0 +1,12 @@
+package mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("index.do")
+	public String indexHandle() {
+		return "index";
+	}
+}
