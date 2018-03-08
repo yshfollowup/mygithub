6e105e90ea5e79dd5215f9bb93474e0d74c86794
admin <admin@1104-25-PC>
2018-03-08 11:14:22 +0900

    project ver 0.1.2

--- WebContent/WEB-INF/views/index.jsp
+++ WebContent/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -11,6 +12,7 @@
 	<div>
 		<b>이제부터 시작!</b>
 		<hr/>
+		<c:out value="${ment }"></c:out>
 	</div>
 </body>
 </html>
--- src/mvc/controller/IndexController.java
+++ src/mvc/controller/IndexController.java
@@ -1,12 +1,14 @@
 package mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 	@RequestMapping("/index.do")
-	public String indexHandle() {
+	public String indexHandle(Model model) {
+		model.addAttribute("ment", "GIT");
 		return "index";
 	}
 }
